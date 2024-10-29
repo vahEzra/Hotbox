db994e6b51c7bf3f17a42c0d17c1b3ffbb760391
vahEzra <lifecycle702@gmail.com>
2024-10-29 06:05:11 -0400

    Update Loader2.lua

--- main/Loader2.lua
+++ main/Loader2.lua
@@ -9,7 +9,7 @@ local plr = game.Players.LocalPlayer
 print("                                           ")
 print("                                           ")
 print("                                           ")
-print("Hotbox Script Experience Version.1.2")
+print("Hotbox Script Experience Version.1.3")
 print("Thanks for Installing Hotbox.")
 wait(3)
 print("Loading Account and Executor Information.")
@@ -29,7 +29,7 @@ print("Hotbox Seems to Be in: " .. game.Name)
 print("Hotbox Searching for Any Hubs with GameID: " .. game.GameId)
 
 wait(6)
-if game.GameId == 6516141723 then
+if game.GameId == 2440500124 then
 	warn("loadstring WEBSITES: :https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Loader.lua: or :https://pastebin.com/raw/wbJTDuKw: ")
 elseif game.GameId == 2788229376 then
 	warn("(spaceX is here but daHood added anti-cheat and if you cheat you get perma banned BE WARNED!) loadstring WEBSITES: https://raw.githubusercontent.com/spacexrandom/Lua/main/DaHood")
